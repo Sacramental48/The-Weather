e75786493a680339fa719c93a6d7a5e0e0e5fb07
Sacramental48 <lomberjos33hg@gmail.com>
2022-12-21 17:58:51 +0300

    redesign layout and done output of the temperature

--- src/components/TheCurrentDay.vue
+++ src/components/TheCurrentDay.vue
@@ -1,107 +1,65 @@
 <template>
     <div class="wrapper">
         <div class="current-day">
-            <div class="current-day__city center">London</div>
-            <div class="current-day__discription center">Yasno</div>
-            <div class="current-day__temperature center">28 &#8451;</div>
-            <div class="current-day__month center">Tuesday, 25 feb</div>
-        </div>
-        <div class="another-day">
-                <!-- <div class="another-day__one box">
-                    <div class="left">
-                        <p>Monday</p>
-                        <span>clear</span>
-                    </div>
-                    <div class="rigth">
-                        <div>25 <span>&#8451;</span></div>
-                    </div>
-                </div>
-                <div class="another-day__two box">
-                    <div class="left">
-                        <p>Wednesday</p>
-                        <span>clear</span>
-                    </div>
-                    <div class="rigth">
-                        <div>25 <span>&#8451;</span></div>
-                    </div>
-                </div>
-                <div class="another-day__three box">
-                    <div class="left">
-                        <p>Thursday</p>
-                        <span>rain</span>
-                    </div>
-                    <div class="rigth">
-                        <div>25 <span>&#8451;</span></div>
-                    </div>
-                </div>
-                <div class="another-day__four box">
-                    <div class="left">
-                        <p>Friday</p>
-                        <span>rain</span>
-                    </div>
-                    <div class="rigth">
-                        <div>25 <span>&#8451;</span></div>
-                    </div>
-                </div>
-                <div class="another-day__five box">
-                    <div class="left">
-                        <p>Saturday</p>
-                        <span>snow</span>
-                    </div>
-                    <div class="rigth">
-                        <div>25 <span>&#8451;</span></div>
-                    </div>
-                </div>
-                <div class="another-day__six box">
-                    <div class="left">
-                        <p>Sunday</p>
-                        <span>rain</span>
-                    </div>
-                    <div class="rigth">
-                        <div>25 <span>&#8451;</span></div>
-                    </div>
-                </div>
-            </div> -->
-            <div class="city">
-                <h1 class="city-discribe">Enter City!</h1>
-                <div class="search">
-                    <input type="text" id="city" class="city-name" placeholder="City" v-model="city">
-                </div>  
-                <div class="button" @click="getRequest">
-                    <div class="button__text">Search!</div>
-                    <div class="button__wrapper">
-                        <div class="button__arrow"></div>
-                        <div class="button__border-circle"></div>
-                        <div class="button__mask-circle">
-                            <div class="button__small-circle"></div>
-                        </div>
-                    </div>
-                </div>
+            <div v-if="data !== null">
+                <div class="current-day__city center">City-name</div>
+                <div class="current-day__discription center">Yasno</div>
+                <div class="current-day__temperature center">{{data.temp}}&#8451;</div>
+                <div class="current-day__month center">Tuesday, 25 feb</div>
             </div>
+            <div v-else>
+                <h1>Choose your city</h1>
+            </div>
+        </div>
+        <div class="city">
+            <h1 class="city-discribe">Enter City!</h1>
+            <div class="search">
+                <input type="text" class="city-name" placeholder="...London?" v-model="city" @keyup.enter="getRequest">
+                <small class="error" v-if="isError">Введите корректное название города!</small>
+            </div>  
+            <button class="btn" :disabled="isActive" @click="getRequest">Search</button>
         </div>
     </div>
 </template>
 
 <script>
-import {ref} from 'vue'
+import {ref, watch} from 'vue'
 import axios from 'axios'
 export default {
     setup() {
         const city = ref('')
-        const res = ref('')
-        console.log(process.env.VUE_APP_API_KEY)
+        const data = ref(null)
+        const isError = ref(false)
+        const isActive = ref(true)
+
+        watch(isError, val => {
+            if(val) {
+                setTimeout(() => {
+                    error.value = false
+                }, 3000)
+            }
+        })
+
+        watch(city, val => {
+            if(val.length > 3) {
+                isActive.value = false
+            }
+        })
+
         const getRequest = async () => {
-            const url = `https://api.api-ninjas.com/v1/weather?city=${city.value}`
-            await axios.get(
-                url, {
-                headers: {
-                    'X-Api-Key': 'ZX5zAsrMA/cgYkC8dY2X9g==iWYkq7v5ziXfLFi9'
-                },
-            }).then( response => res.value = response.data)
-            console.log(res.value)
+            try {
+                const url = `https://api.api-ninjas.com/v1/weather?city=${city.value}`
+                await axios.get(url, {
+                    headers: {
+                        'X-Api-Key': process.env.VUE_APP_API_KEY
+                    },
+                }).then(res => data.value = res.data)
+            }catch(e) {
+                isError.value = true
+            }
         }
         return {
-            city, getRequest, res
+            city, getRequest, isError, data, isActive
         }
     }
 }
--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="wrapper">
+    <div>
         <TheCurrentDay></TheCurrentDay>
     </div>
 </template>
